ce4d7fcb40ac3951ac4b9a9c5bcd80627908a59a
kübra güçyeter <kubra.gucyeter01@gmail.com>
2024-02-02 15:04:04 +0300

    Projects I did to learn

---
+++ app_promise.js
@@ -0,0 +1,45 @@
+
+
+// const check = true;
+
+// const promise1 = new Promise((resorve, reject) => {
+//     if (check) {
+//         resorve("Promise Başarılı")
+//     } else {
+//         reject("Promise Başarasız..");
+//     }
+// })
+
+// console.log(promise1)
+
+
+TODO://Asenkron yapıları senkrona çevirmek için kullanıyoruz
+TODO //CALLBACK'leri alternafi 
+TODO //ES6 
+
+// let check = true;
+// function createPromise() {
+//     return new Promise((resolve, reject) => {
+//         if (check) {
+//             resolve("Promis'te herhangi bir sıkıntı yok")
+//         } else {
+//             reject("Sıkıntı büyük")
+//         }
+//     })
+// }
+
+// createPromise()
+//     .then((response) => {
+//         console.log(response);
+//     })
+
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+//     .finally(() => console.log("Her zaman çalışır"))
+
+
+// ? PROMİSE + XMLHTTPREQUEST
+
+   
